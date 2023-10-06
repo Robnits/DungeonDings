22c4ea825637d79f81036712f87235d5ff2333d6
IchBinGavin <144715102+IchBinGavin@users.noreply.github.com>
2023-10-06 15:02:51 +0200

    ScriptsRats
    Rats Scripts

---
+++ Assets/scripts/Enemy/Rats.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.Animations;
+using UnityEngine.UIElements;
+
+public class Rats : MonoBehaviour
+{
+
+    public GameObject player;
+
+
+    [SerializeField]
+    private FloatSO ScoreSO;
+
+    private float speed = 3f;
+    private float life = 3f;
+    private float value = 10f;
+    private float damage = 3f;
+    public Transform target;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (life <= 0)
+            Death();
+
+        transform.LookAt(target.position);
+        transform.Rotate(new Vector2(0, -90), Space.Self);//correcting the original rotation
+
+
+        //move towards the player
+        if (Vector3.Distance(transform.position, target.position) > 1f)
+        {
+            transform.Translate(new Vector2(speed * Time.deltaTime, 0));
+
+        }
+    }
+
+        void Death()
+    {
+        ScoreSO.NewMoney += value;
+        Destroy(gameObject);  
+    }
+
+    public float GetDamage()
+    {
+        return damage;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            life -= player.GetComponent<Player_behjaviour>().GetDamage();
+            Debug.Log(life);
+        }
+    }
+
+}
