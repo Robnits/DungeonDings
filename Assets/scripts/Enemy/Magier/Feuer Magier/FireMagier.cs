095aac93eb8b0f35ba5ff1578d163d68553b4886
Robnits <98857700+Robnits@users.noreply.github.com>
2024-03-12 20:08:27 +0100

    for marcel

--- Assets/scripts/Enemy/Magier/Feuer Magier/FireMagier.cs
+++ Assets/scripts/Enemy/Magier/Feuer Magier/FireMagier.cs
@@ -9,49 +9,39 @@ public class Firemagier : EnemysHauptklasse
     private Rigidbody2D rb;
 
     public bool playerIsInRange;
-    public bool playerIsInMelee;
     void Start()
     {
         life = 5f;
         value = 10f;
-        speed = 4f;
+        speed = 2.5f;
         droprate = 90;
         healthscript.GetMaxhealth(life);
         player = GameObject.FindGameObjectWithTag("Player");
         rb = GetComponent<Rigidbody2D>();
     }
-   
-   
-    void Update()
-    {
-        
-    }
+
     void FixedUpdate()
     {
         if (player != null)
         {
+            float distance = Vector2.Distance(gameObject.transform.position, player.transform.position);
 
-            if (playerIsInRange)
+            Vector3 directionToPlayer = player.transform.position - transform.position;
+
+            // Calculate the angle to rotate towards the player
+            float angle = Mathf.Atan2(directionToPlayer.y, directionToPlayer.x) * Mathf.Rad2Deg + 90;
+
+            // Rotate the enemy to face the player
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            if (distance < 2.1)
             {
-                float distance = Vector2.Distance(gameObject.transform.position, player.transform.position);
-
-                Vector3 directionToPlayer = player.transform.position - transform.position;
-
-                // Calculate the angle to rotate towards the player
-                float angle = Mathf.Atan2(directionToPlayer.y, directionToPlayer.x) * Mathf.Rad2Deg + 90;
-
-                // Rotate the enemy to face the player
-                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-                if (distance <= 3)
-                {
-                    StartCoroutine(MoveBackwards());
-                }
-                else
-                {
-                    Vector2 moveDirection = (player.transform.position - transform.position).normalized;
-                    rb.velocity = moveDirection * speed;
-                }
+                StartCoroutine(MoveBackwards());
+            }
+            else
+            {
+                Vector2 moveDirection = (player.transform.position - transform.position).normalized;
+                rb.velocity = moveDirection * speed;
             }
         }
     }
@@ -68,21 +58,6 @@ public class Firemagier : EnemysHauptklasse
             yield return null;
         }
 
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            playerIsInRange = false;
-        }
-    }
-    private new void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player") && playerIsInMelee == false)
-        {
-            playerIsInRange = true;
-            
-        }
     }
 }
 
