e8c83174efd38bd3c76f6dcf5f016795940d6469
Robnits <98857700+Robnits@users.noreply.github.com>
2024-04-19 02:39:08 +0200

    19.4 bug fixes and neeko

--- Assets/scripts/Bosses/Neeko.cs
+++ Assets/scripts/Bosses/Neeko.cs
@@ -13,6 +13,13 @@ public class Neeko : BossHauptklasse
     public GameObject firePoint;
     public GameObject NeekoProjectile;
     private bool canshootagain = true;
+    private bool isMovingRandomly;
+    private Vector2 randomMoveDirection;
+    private float randomMoveDuration = 1.0f;
+    private float randomMoveTimer = 1f;
+
+    public bool playerIsInRange;
+    public float delay = 0.0f;
 
     private void Start()
     {
@@ -22,6 +29,7 @@ public class Neeko : BossHauptklasse
         player = GameObject.FindGameObjectWithTag("Player");
         life = 100f;
         damage = 5;
+        speed = 2.2f;
         SetHealthbar();
     }
 
@@ -39,23 +47,66 @@ public class Neeko : BossHauptklasse
                     
                 break;
             case 1:
-                transform.position = rotationPoint.transform.position;
-                Vector2 aimdirection = new Vector2(player.transform.position.x,player.transform.position.y) - rb.position;
-                float aimangle = Mathf.Atan2(aimdirection.y, aimdirection.x) * Mathf.Rad2Deg + 90;
-                rb.rotation = aimangle;
                 if(canshootagain)
+                    transform.position = rotationPoint.transform.position;
+                    LookAtPlayer();
                     StartCoroutine(Shoot());
                 break;
+            case 2:
+                if (isMovingRandomly)
+                {
+                    randomMoveTimer += Time.deltaTime;
+                    LookAtPlayer();
+                    if (randomMoveTimer < randomMoveDuration)
+                        rb.velocity = randomMoveDirection * speed;
+                    else
+                    {
+                        isMovingRandomly = false;
+                        rb.velocity = Vector2.zero;
+                    }
+                }
+                break;
+        }
+    }
+    private void FixedUpdate() {
+        if (BattlePhase == 2)
+        {
+            if (player != null)
+        {
+            float distance = Vector2.Distance(transform.position, player.transform.position);
+
+            if (distance == 1.8)
+            {
+                StartCoroutine(MoveBackwards());
+            }
+            else
+            {
+                if (distance > 3.8)
+                {
+                    Vector2 moveDirection = (player.transform.position - transform.position).normalized;
+                    rb.velocity = moveDirection * speed;
+                    StartCoroutine(MoveForwards());
+                }
+                else
+                    StartCoroutine(RandMove());
+            }
+        }
+
         }
     }
 
     public void StartRotate(GameObject phaseRotationPoint)
     {
-        
         rotationPoint = phaseRotationPoint;
         BattlePhase++;
     }
 
+    private void LookAtPlayer()
+    {
+        Vector2 aimdirection = new Vector2(player.transform.position.x,player.transform.position.y) - rb.position;
+        float aimangle = Mathf.Atan2(aimdirection.y, aimdirection.x) * Mathf.Rad2Deg + 90;
+        rb.rotation = aimangle;
+    }
 
     IEnumerator Shoot()
     {
@@ -65,4 +116,58 @@ public class Neeko : BossHauptklasse
         bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.transform.up * 2, ForceMode2D.Force);
         StartCoroutine(Shoot());
     }
+
+    IEnumerator MoveBackwards()
+    {
+        float backwardDuration = 1f;
+        float timePassed = 0;
+        while (timePassed < backwardDuration && player != null)
+        {
+            Vector2 moveDirection = (transform.position - player.transform.position).normalized;
+            rb.velocity = moveDirection * speed;
+            timePassed += Time.deltaTime;
+
+            yield return null;
+        }
+        rb.velocity = Vector2.zero;
+        yield return new WaitForSeconds(delay);
+        
+
+    }
+    IEnumerator MoveForwards()
+    {
+            float forwardDuration = 1f;
+            float timePassed = 0;
+            while (timePassed < forwardDuration && player != null)
+            {
+                Vector2 moveDirection = (player.transform.position - transform.position).normalized;
+                rb.velocity = moveDirection * speed;
+                timePassed += Time.deltaTime;
+
+                yield return null;
+            }
+            rb.velocity = Vector2.zero;
+            yield return new WaitForSeconds(delay);
+    }
+
+    IEnumerator RandMove()
+    {   
+        while (true)
+        {
+            if (!playerIsInRange && !isMovingRandomly)
+            {
+                isMovingRandomly = true;
+                randomMoveDirection = Random.insideUnitCircle.normalized;
+                randomMoveTimer = 0.0f;
+            }
+            else if (playerIsInRange)
+            {
+                isMovingRandomly = false;
+            }
+
+            yield return new WaitForSeconds(delay);
+
+        }
+    }
+
 }
